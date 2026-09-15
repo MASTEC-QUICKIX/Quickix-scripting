7b36f47c2c4716f16bc2d3ed9dfcf91c70950e77
Baby akshatha <102918841+ashhureddy@users.noreply.github.com>
2026-09-15 10:26:50 -0400

    Update run_validation.py

--- run_validation.py
+++ run_validation.py
@@ -56,7 +56,7 @@ def run(ciq_path, edp_path, rfds_path, node_logs_text, out_pdf):
 
     results = {k: [] for k in (
         'sw_version', 'identity', 'primary_secondary', 'board_type', 'xmu',
-        'cells_vs_rfds', 'cell_id_vs_rfds', 'nrcelldu_nrcellcu', 'antenna_type_rfds', 'params_4g', 'params_5g', 'arfcn_bw_5g', 'ssb_5g',
+        'cells_vs_rfds', 'cell_id_vs_rfds', 'nrcelldu_nrcellcu', 'antenna_type_rfds', 'gnb_identity', 'params_4g', 'params_5g', 'arfcn_bw_5g', 'ssb_5g',
         'pci_4g', 'pci_5g', 'radio_type', 'sector_swap', 'radio_sharing',
         'port_uniqueness', 'xmu_port_overlap', 'antenna', 'nbiot', 'nr_tac', 'tac', 'sef_fru',
         'dss', 'sector_id_4890', 'rfbranch_per_aug', 'ptp_matrix', 'losses_vs_antenna',
@@ -96,6 +96,7 @@ def run(ciq_path, edp_path, rfds_path, node_logs_text, out_pdf):
         results['cells_vs_rfds'] += cs.check_cells_vs_rfds(node_id, ciq_wb, rfds_pages, e_name, g_name)
         results['nrcelldu_nrcellcu'] += cs.check_nrcelldu_nrcellcu_match(node_id, ciq_wb, g_name)
         results['antenna_type_rfds'] += cs.check_antenna_type_vs_rfds(node_id, ciq_wb, rfds_pages, g_name)
+        results['gnb_identity'] += cs.check_gnb_identity_consistency(node_id, ciq_wb, g_name)
         results['cell_id_vs_rfds'] += cs.check_cell_id_vs_rfds(node_id, log_text, ciq_wb, rfds_pages, e_name, g_name, node_logs, moved_map)
         results['params_4g'] += cs.check_rf_params_4g(node_id, log_text, ciq_wb, has_pre, retuned_cells, node_logs, moved_map)
         import log_parser as lp
